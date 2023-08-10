04487dcd50bc8e8adf20ff98fb7b12800c8f9afc
Omar Fahmy <Ofahmy1234@gmail.com>
2023-08-10 15:23:24 +0300

    Initial Commit

---
+++ STP'24/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import LandingPage from "./components/members-recruitment/landing-page/landing-page";
+
+function App() {
+    return (
+        <>
+            <div className="App">
+                <h1>STP'24</h1>
+                <LandingPage />
+            </div>
+        </>
+    );
+}
+
+export default App;
