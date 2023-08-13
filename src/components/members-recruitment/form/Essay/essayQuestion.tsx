eacfa9ad4d65b7ff118d98cbd6a31a0819f515f3
Omar Fahmy <Ofahmy1234@gmail.com>
2023-08-13 22:39:51 +0300

    fix: text area memo

--- src/components/members-recruitment/form/Essay/essayQuestion.tsx
+++ src/components/members-recruitment/form/Essay/essayQuestion.tsx
@@ -1,7 +1,7 @@
 import { useFormStore } from '../../../../zustand/form/formStore';
 import './essayQuestion.css'
 export function EssayQuestion({ question, name }: { question: string; name: string }) {
-    const { updateForm } = useFormStore();
+    const { Form, updateForm } = useFormStore();
     function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
         updateForm({[name]: e.target.value});
     }
@@ -11,6 +11,7 @@ export function EssayQuestion({ question, name }: { question: string; name: stri
                 name={name}
                 id=""
                 placeholder={question}
+                value={Form[name as keyof typeof Form]? Form[name as keyof typeof Form] : ""}
                 required
                 onChange={handleChange}
             ></textarea>
