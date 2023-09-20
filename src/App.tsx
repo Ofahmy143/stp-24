725c320ae1cd596810f1fe0fe12869f9f8304386
Omar Fahmy <Ofahmy1234@gmail.com>
2023-09-20 03:08:01 +0300

    feat: add routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import "./App.css";
-import Form from "./components/members-recruitment/form/form";
-import LandingPage from "./components/members-recruitment/landing-page/landing-page";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+// import Form from "./components/members-recruitment/form/form";
+// import LandingPage from "./components/members-recruitment/landing-page/landing-page";
+import { LandingPage as OrientationLandingPage } from "./routes/orientation/landingPage/LandingPage.route";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { OrientationForm } from "./routes/orientation/orientationForm/orientationForm.route";
 function App() {
     return (
         <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<OrientationLandingPage />} />
+                    <Route
+                        path="/orientation"
+                        element={<OrientationLandingPage />}
+                    />
 
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<LandingPage />} />
-                        <Route path="/recruitment-form" element={<Form />} />
-                    </Routes>
-                </Router>
-            
+                    <Route
+                        path="/orientation/form"
+                        element={<OrientationForm />}
+                    />
+
+                    {/* <Route path="/recruitment-form" element={<Form />} /> */}
+                </Routes>
+            </Router>
         </>
     );
 }
