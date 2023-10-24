8c19256d8b3b3a15d6ee325a949207be398c5a5a
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2023-10-24 17:26:26 +0300

    Adding Page Contents

---
+++ src/components/Macathon-Components/form/questions.ts
@@ -0,0 +1,98 @@
+export type Question = {
+  title: string;
+  pageQuestions: PageQuestion[];
+};
+
+enum QuestionTypeEnum {
+  NORMAL_TEXT = "text",
+  EMAIL = "email",
+  NUMBER = "number",
+  FILE_UPLOAD = "file",
+  SELECT = "select",
+  ESSAY = "essay",
+}
+
+export type PageQuestion = {
+  question: string;
+  type: QuestionTypeEnum;
+  name: string;
+  regex?: string;
+  choices?: string[];
+  subChoices?: { [key: string]: string[] };
+  placeholder?: string;
+};
+
+export type QuestionsObject = { [key: string]: Question };
+
+export const questions: QuestionsObject = {
+  "mainPage": {
+    title: "",
+    pageQuestions: [
+      {
+        question: "full name",
+        type: QuestionTypeEnum.NORMAL_TEXT,
+        name: "name",
+        regex: "",
+      },
+      {
+        question: "Email",
+        type: QuestionTypeEnum.EMAIL,
+        name: "email",
+        regex: "",
+      },
+      {
+        question: "Phone Number (Whatsapp No.)",
+        type: QuestionTypeEnum.NORMAL_TEXT,
+        name: "mobile",
+        regex: "",
+      },
+      {
+        question: "University",
+        type: QuestionTypeEnum.NORMAL_TEXT,
+        name: "University",
+        regex: "",
+      },
+      {
+        question: "Faculty",
+        type: QuestionTypeEnum.NORMAL_TEXT,
+        name: "Faculty",
+        regex: "",
+      },
+      {
+        question: "Graduation Year",
+        type: QuestionTypeEnum.NUMBER,
+        name: "Graduation Year",
+        regex: "",
+      },
+    ],
+  },
+  "secondPage": {
+    title: "Team leader",
+    pageQuestions: [
+      {
+        question: "CV (google drive link)",
+        type: QuestionTypeEnum.FILE_UPLOAD,
+        name: "CV",
+        regex: "",
+      },
+    ],
+  },
+  "lastPage": {
+    title:"",
+    pageQuestions: [
+      {
+        question: "How did you know about the competition?",
+        type: QuestionTypeEnum.ESSAY,
+        name: "essayQuesOne",
+        regex: "",
+      },
+      {
+        question: "Why do you want to join the competition?",
+        type: QuestionTypeEnum.ESSAY,
+        name: "essayQuesTwo",
+        regex: "",
+      },
+    ],
+  },
+};
+
