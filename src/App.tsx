bf0af1c865a5e7db411adc98e41889af8eec6911
Omar Fahmy <ofahmy1234@gmail.com>
2023-10-27 03:31:28 +0300

    add hash router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import Form from "./components/members-recruitment/form/form";
 // import LandingPage from "./components/members-recruitment/landing-page/landing-page";
 // import { LandingPage as OrientationLandingPage } from "./routes/orientation/landingPage/LandingPage.route";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 // import { OrientationForm } from "./routes/orientation/orientationForm/orientationForm.route";
 import WorkShopForm from "./routes/WorkshopForm/WorkshopForm";
 function App() {
